c6c30a0814bdb237ea020ad2bac2b8b3c572eb8f
LukaMrt <maret.luka@gmail.com>
2020-02-08 19:48:56 +0100

    update presenter

--- src/main/java/fr/lukam/clock/infrastructure/presenters/BerlinClockPresenter.java
+++ src/main/java/fr/lukam/clock/infrastructure/presenters/BerlinClockPresenter.java
@@ -15,8 +15,25 @@ public class BerlinClockPresenter implements ClockPresenter {
 
         String[] timeArray = time.split(" ");
 
-        viewModel.text = timeArray;
+        String seconds = timeArray[0];
+        String firstHoursLine = timeArray[1];
+        String secondHoursLine = timeArray[2];
+        String firstMinutesLine = timeArray[3];
+        String secondMinutesLine = timeArray[4];
 
+        seconds = "               " + seconds;
+        firstHoursLine = String.join("      ", firstHoursLine.split(""));
+        secondHoursLine = String.join("      ", secondHoursLine.split(""));
+        firstMinutesLine = String.join("  ", firstMinutesLine.split(""));
+        secondMinutesLine = String.join("      ", secondMinutesLine.split(""));
+
+        timeArray[0] = seconds;
+        timeArray[1] = "    " + firstHoursLine;
+        timeArray[2] = "    " + secondHoursLine;
+        timeArray[3] = firstMinutesLine;
+        timeArray[4] = "    " + secondMinutesLine;
+
+        viewModel.text = timeArray;
     }
 
 }
